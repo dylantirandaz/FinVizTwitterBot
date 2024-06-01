4ccfec48e9c845939d01f91a870b8e8f98898fda
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2024-05-31 21:42:47 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Twitter API
+API_KEY = ' '
+API_SECRET_KEY = ' '
+ACCESS_TOKEN = ' '
+ACCESS_TOKEN_SECRET = ' '
+
+# Misc
+INSIDER_SALES_URL = 'https://finviz.com/insidertrading.ashx'
+MIN_SALE_AMOUNT = 1_000_000
